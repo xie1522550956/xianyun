42519f90c7124ba860375c99c29268520d1ec584
xiegege <1522550956@qq.com>
2019-12-04 20:37:48 +0800

    机票列表页显示完成

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -1,10 +1,32 @@
 <template>
-  <div>123</div>
+  <div class="flights">
+      <flightsListHead ></flightsListHead>
+      <flightsItem v-for="(value,index) in flights" :key='index' :flight='flights[index]'></flightsItem>
+  </div>
 </template>
 
 <script>
+import flightsListHead from '@/components/air/flightsListHead'
+import flightsItem from '@/components/air/flightsItem'
 export default {
-
+    data () {
+        return {
+            flights: []
+        }
+    },
+    async mounted () {
+        
+        console.log(this.$route.query)
+        let res = await this.$axios({
+            url:'/airs',
+            params: this.$route.query
+        }) 
+        console.log(res)
+        this.flights = res.data.flights.splice(0,10)
+    },
+    components: {
+        flightsListHead,flightsItem
+    }
 }
 </script>
 
